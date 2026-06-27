a2cf7c2c969fd55695c841401b172928fe7c9b18
Danyaell <danyaellmtzo17@gmail.com>
2026-06-27 12:25:46 -0600

    init-commit

---
+++ src/main/java/com/danyaell/mavericklabsbe/MaverickLabsBeApplication.java
@@ -0,0 +1,13 @@
+package com.danyaell.mavericklabsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverickLabsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaverickLabsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danyaell/mavericklabsbe/MaverickLabsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danyaell.mavericklabsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverickLabsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
